8a52aa8e038e47da7d4a0a60ab23ff84a31ee532
vitoria-lara <vitoria.eduarda.lara@escola.pr.gov.br>
2026-03-13 12:02:48 -0300

    templante final

--- template-strings.js
+++ template-strings.js
@@ -1,1 +1,8 @@
-const nome = "J
+const nome = "Ju";
+const idade = 2026-1981;
+const cidadeDeNascimento = "Pitanga";
+
+//const apresentacao = "Meu nome é "+ nome + "minhanidade é" + idade + " e nascida na cidade de" + cidadeDeNascimento;
+
+const apresentacao = 'meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}'
+console.log(apresentacao);
