857ca191133c8d1ccfb4256c7ab4223bc08c2222
vitoria-lara <vitoria.eduarda.lara@escola.pr.gov.br>
2026-03-31 10:33:59 -0300

    parametros

--- parametros.js
+++ parametros.js
@@ -5,3 +5,9 @@ function soma(numero1, numero2){
 console.log (soma(2, 2));
 console.log (soma(245, 20));
 console.log (soma(-500, 60));
+
+function nomeIdade(nome, idade){
+    return 'meu  nome é ${nome} e minha idade é${idade}'
+}
+
+console.log(nomeIdade("Vitoria", 16))
