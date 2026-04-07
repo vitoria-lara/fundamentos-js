c17d5ec9ee1b49e7bb15cff217753a787bc9baf6
vitoria-lara <vitoria.eduarda.lara@escola.pr.gov.br>
2026-04-07 10:35:17 -0300

    expressao

---
+++ expressao-funcao.js
@@ -0,0 +1,28 @@
+//declaração de função
+
+function minhaFuncao(){
+// bloco de código
+}
+
+minhaFuncao("param");
+
+//expressão de função
+
+const soma = function(num1, num2){
+    return num1 + num2
+}
+
+console.log(soma(1, 15));
+
+//diferença principal: Hoisting
+//função e var são "listadas" no top do arquivo.
+
+console.log(aprensentar())
+
+functionapresentar(){
+    return "olá";
+}
+console.log(somaOutroExemplo(1,1))
+const somaOutroExemplo = function (num1, num2){
+    return num1 = num2
+}
