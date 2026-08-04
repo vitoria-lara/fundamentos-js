eea6a53851f562a7788d01c4458854c75169519e
vitoria-lara <vitoria.eduarda.lara@escola.pr.gov.br>
2026-08-04 15:06:43 +0000

    acessoponto

---
+++ objetos/acesso_ponto.js
@@ -0,0 +1,9 @@
+const cliente ={
+    nome: "Vitoria",
+    idade: 16;
+    cpf: "1122233345",
+    email: "Vitoria@DOMImplementation.com",
+};
+
+console.log('O nome do cliente é $ {cliente.nome} e essa pessoa tem $ {cilente idade} anos.');
+console.log('Os 3 primeiros digitos do CPF são {cliente.cpf.substring(0, 3)');
--- objetos/objeto.js
+++ objetos/objeto.js
@@ -1,3 +1,10 @@
 const listaCPF = [1111111, 222222, 3333];
 
-const informacoesPessoa = ["noem", "jose", "idade"]
+const informacoesPessoa = ["noem", "Jose", "idade", "34", "CPF"]
+
+console.log(informacoesPessoa[1]);
+
+const objetoPessoa ={
+    idade = 16,
+    nome: "Vitoria",
+}
